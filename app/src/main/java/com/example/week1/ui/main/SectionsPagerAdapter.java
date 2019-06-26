7d908afea92c4ec0314aa1980915ffa136ad8b21
KevinMIN95 <alsehdcks95@kaist.ac.kr>
2019-06-26 16:05:07 +0900

    practice

--- app/src/main/java/com/example/week1/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/week1/ui/main/SectionsPagerAdapter.java
@@ -41,6 +41,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
+        ////////
         return 3;
     }
 }
