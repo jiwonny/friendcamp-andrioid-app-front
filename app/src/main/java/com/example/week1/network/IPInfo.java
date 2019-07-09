361ea8c65da4dc7d6c3d46d6a99262ac72897972
KevinMIN95 <alsehdcks95@kaist.ac.kr>
2019-07-09 12:18:44 +0900

    Tab3 7/9 Comment

--- app/src/main/java/com/example/week1/network/IPInfo.java
+++ app/src/main/java/com/example/week1/network/IPInfo.java
@@ -1,6 +1,6 @@
 package com.example.week1.network;
 
 public class IPInfo {
-    public String IPAddress = "143.248.39.49";
+    public String IPAddress = "13.125.250.175";
     public int Port = 4500;
 }
